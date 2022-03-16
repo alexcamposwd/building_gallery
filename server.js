8a9f8eb03c03d78fa9422b1a61fc5943dc145c6a
Alex Campos <alexfilhotes@hotmail.com>
2022-03-16 11:34:35 -0300

    Ajustes para deploy

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const jsonServer = ('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-	static: './build'
-});
+const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 5000;
 
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-	'/api/*': '/$1',
-}))
 server.use(router);
 server.listen(PORT, () => {
 	console.log(`Server is running ${PORT}`)
